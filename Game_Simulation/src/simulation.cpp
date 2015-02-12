651d2a8b9808ec13bc618b5e5fd4d61453ea6033
Jkholodnov <jkholodnov@gmail.com>
2015-02-12 01:49:03 -0500

    100 sims multithreaded

--- Game_Simulation/src/simulation.cpp
+++ Game_Simulation/src/simulation.cpp
@@ -14,7 +14,7 @@ void simulation::simulate_players_performance(const unordered_map<string, statis
 	auto threadID = std::this_thread::get_id();
     auto thread_Hash = std::hash<std::thread::id>()(threadID);
     srand(thread_Hash);
-    int performance = rand() %100;
+    int performance;
 
     //TODO: Check if the performance in top 10% or bottom 10%
     /*
@@ -31,11 +31,12 @@ void simulation::simulate_players_performance(const unordered_map<string, statis
    	*/
 
     for(auto key: keys_to_map){
+    	performance = rand() %100;
     	auto statistics_mean_and_stdev =  mean_and_stdevs.find(key);
     	double mean = (statistics_mean_and_stdev->second).mean;
     	double stdev = (statistics_mean_and_stdev->second).stdev;
     	string RInside_Query = "qnorm(." + to_string(performance) + ", mean = " + to_string(mean) + ", stdev = " + to_string(stdev) + ")";
-    	cout << "Mean " << mean << " Stdev " << stdev << "Query " << RInside_Query << endl;
+    	//cout << "Mean " << mean << " Stdev " << stdev << "Query " << RInside_Query << endl;
     	auto predicted_value = R_Inside_Container->use(RInside_Query);
     	simulated_performance.emplace(key,predicted_value);
     }
