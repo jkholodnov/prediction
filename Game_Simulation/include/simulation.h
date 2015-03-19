5547e13a3c11bde724ad099654ec51e77a9e9e98
Jkholodnov <jkholodnov@gmail.com>
2015-03-19 00:57:53 -0400

    priv/pub

--- Game_Simulation/include/simulation.h
+++ Game_Simulation/include/simulation.h
@@ -14,10 +14,11 @@ class simulation
         virtual ~simulation();
         void simulate_players_performance(const unordered_map<string, statistics>& mean_and_stdevs, const vector<string>& keys_to_map, shared_ptr<RInside_Container> R_Inside_Container, string playname);
         int get_simulated_value(string key);
-    protected:
-    private:
+    
         unordered_map<string, double> simulated_performance{};
         string predicted_injury_status;
+    protected:
+    private:
 };
 
 #endif // SIMULATION_H
