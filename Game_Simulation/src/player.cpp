f3b06b96aa56152f55728b61295a9c6b5de95b4a
Jkholodnov <jkholodnov@gmail.com>
2015-03-24 17:59:22 -0400

    fixed lm output

--- Game_Simulation/src/player.cpp
+++ Game_Simulation/src/player.cpp
@@ -164,7 +164,7 @@ pair<int,int> player::simulate_game_scores(int i){
     cout << player_name << " mins: " << minutes_value << " fga: " << fga_value << " tpa: " << tpa_value<< " fta: " <<fta_value<< " oreb: " <<oreb_value<< " ast: " <<assist_value<< " stl: " <<steal_value<< " to: " <<turnover_value<< " fl: " <<fouls_value<< " +-: " <<plus_minus_value<<endl;
     
     //These values are hard coded from my lm(Score ~ .) output. Need to figure out a way to route RInside to here.
-    double predicted_score = -0.58433440 + 0.03139078*minutes_value + 0.95074756*fga_value + 0.20738638*tpa_value + 0.78631074*fta_value - 0.07971875*oreb_value - 0.15509996*assist_value - 0.09794145*steal_value + 0.10168076*turnover_value + 0.03700077*fouls_value + 0.08231120*plus_minus_value; 
+    double predicted_score = -0.520640 + 0.027873*minutes_value + 0.959890*fga_value + 0.206571*tpa_value + 0.793915*fta_value - 0.115459*oreb_value - 0.153142*assist_value - 0.077242*steal_value + 0.107611*turnover_value + 0.027402*fouls_value + 0.087565*plus_minus_value; 
     
     return make_pair(round(minutes_value), round(predicted_score));
 }
