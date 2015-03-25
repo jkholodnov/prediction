3411fb06e6d076ce6931dd7dd1e17d640d81980f
Jkholodnov <jkholodnov@gmail.com>
2015-03-25 16:09:49 -0400

    Async Parallelization of parseGames

--- Team_Elo/src/games_and_teams.cpp
+++ Team_Elo/src/games_and_teams.cpp
@@ -21,39 +21,57 @@ void games_and_teams::initialize_teams(){
 }
 void games_and_teams::get_games()
 {
-    string _query = "SELECT gameId, Team1Abbr, Team2Abbr, Team1Score, Team2Score FROM games ORDER BY day ASC;";
+    
+    string _query = "SELECT DISTINCT day FROM games;";
+    auto game_days = the_db->query(_query);
 
-    auto games_info = the_db->query(_query);
-    int _score1, _score2;
+    for(auto& day : game_days){
+        vector<game> games_on_this_day;
+        _query = "SELECT gameId, Team1Abbr, Team2Abbr, Team1Score, Team2Score FROM games where day = '" + day[0] + "';";
+        auto games_on_day = the_db->query(_query);
+        for(auto& game : games_on_day){
+            int _score1, _score2;
+            team* team1;
+            team* team2;
 
-    for(auto& game: games_info)
-    {
-        team* team1;
-        team* team2;
-
-        for(auto& team : the_teams){
-            if(team.getAbbr() == game[1]){
-                team1 = &team;
+            for(auto& team : the_teams){
+                if(team.getAbbr() == game[1]){
+                    team1 = &team;
+                }
+                else if(team.getAbbr() == game[2]){
+                    team2 = &team;
+                }
             }
-            else if(team.getAbbr() == game[2]){
-                team2 = &team;
-            }
+            
+            _score1 = atoi(game[3].c_str());
+            _score2 = atoi(game[4].c_str());
+
+            games_on_this_day.emplace_back(game[0], team1, team2, _score1, _score2);
         }
-        
-        _score1 = atoi(game[3].c_str());
-        _score2 = atoi(game[4].c_str());
-
-        the_games.emplace_back(game[0], team1, team2, _score1, _score2);
+        the_games.emplace_back(games_on_this_day);
     }
-    cout << "Pulled all games. There are " << the_games.size() << " games to parse." << endl;
+    cout << "Pulled all games. There are " << the_games.size() << " days of games to parse." << endl;
 }
 
 void games_and_teams::parseGames()
 {
+    size_t i;
+    vector<future<int>> this_days_games(game_day.size());
+
     int Number_Correct_Ranking{0};
 
-    for(auto& game:the_games)
+    for(auto& game_day:the_games)
     {
+        vector<future<int>> this_days_games(game_day.size());
+        for(i=0; i<game_day.size(); i++){
+            this_days_games[i] = async( launch::async, boost::bind( &game::update_Team_Ratings, game_day[i] ));
+        }
+
+        /*
+        for(auto& game : game_day){
+            schedule(game_parser_threads, boost::bind(&game::update_Team_Ratings, &game))
+        }
+        */
         Number_Correct_Ranking += game.update_Team_Ratings(the_db);
     }
     cout << Number_Correct_Ranking << endl;
