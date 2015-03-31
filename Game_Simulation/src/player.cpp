8d4fe34ddd0b2bf9b2e03d76cdee8242769c15b7
Jkholodnov <jkholodnov@gmail.com>
2015-03-31 15:55:08 -0400

    smallfix

--- Game_Simulation/src/player.cpp
+++ Game_Simulation/src/player.cpp
@@ -116,7 +116,7 @@ void player::get_player_scores(shared_ptr<RInside_Container> R_Inside_Container)
     size_t i;
     for(i=0;i<100;i++){
     	game_simulations.emplace_back();
-        worker_threads.emplace_back(simulation::simulate_players_performance, game_simulations[i], mean_and_stdevs, keys_to_map, R_Inside_Container, player_name);
+        worker_threads.emplace_back(&simulation::simulate_players_performance, game_simulations[i], mean_and_stdevs, keys_to_map, R_Inside_Container, player_name);
     }
     for(i=0; i< worker_threads.size(); i++){
         worker_threads[i].join();
