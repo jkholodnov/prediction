63bce01ae6a38d996a879d9d47da621f3e597eb9
Jkholodnov <jkholodnov@gmail.com>
2015-03-31 15:56:58 -0400

    smallfix

--- Game_Simulation/src/player.cpp
+++ Game_Simulation/src/player.cpp
@@ -131,6 +131,7 @@ void player::get_player_scores(shared_ptr<RInside_Container> R_Inside_Container)
 }
 
 int player::simulate_game_scores(int i){
+    cout << "called" << endl;
     auto the_simulation = game_simulations[i];
     auto map_of_performances = the_simulation.simulated_performance;
 
