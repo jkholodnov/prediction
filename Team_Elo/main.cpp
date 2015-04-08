1a563c2f49a6c66626df594b197d47b76fafe22d
Jkholodnov <jkholodnov@gmail.com>
2015-04-08 11:15:26 -0400

    endl's to the main couts

--- Team_Elo/main.cpp
+++ Team_Elo/main.cpp
@@ -18,12 +18,13 @@ int main() {
 
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
     duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-    cout << "The program took " << time_span.count() << " seconds to generate ELO.";
+    cout << "The program took " << time_span.count() << " seconds to generate ELO."
+         << endl;
 
     all_games.generate_Performance_Rating();
 
     high_resolution_clock::time_point t3 = high_resolution_clock::now();
     time_span = duration_cast<duration<double>>(t3 - t2);
     cout << "The program took " << time_span.count()
-         << " seconds to generate Performance Rating.";
+         << " seconds to generate Performance Rating." << endl;
 }
