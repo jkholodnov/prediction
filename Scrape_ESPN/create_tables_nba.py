027af3c6aaae891c2c85760f075900dc7c12e2b5
jkholodnov <jkholodnov@gmail.com>
2015-04-26 19:18:49 -0400

    Players Table Updated: Rosters, Salaries, Player Height, and Player Weight were added.

--- Scrape_ESPN/create_tables_nba.py
+++ Scrape_ESPN/create_tables_nba.py
@@ -2,20 +2,27 @@ import sqlite3 as lite
 con = lite.connect('predict.db')
 
 with con:
-
     cur = con.cursor()
+
     cur.execute(
         "CREATE TABLE games(gameID INT PRIMARY KEY NOT NULL, " +
-        "day DATE, Team1Abbr TEXT, Team2Abbr TEXT, Team1Score INT, " +
-        "Team2Score INT, Team1ELO INT, Team2ELO INT)")
+                            "day DATE, Team1Abbr TEXT, Team2Abbr TEXT, Team1Score INT, " +
+                            "Team2Score INT, Team1ELO INT, Team2ELO INT)")
     cur.execute(
         "CREATE TABLE players(playerID INTEGER PRIMARY KEY NOT NULL, " +
-        "teamID TEXT, Name TEXT, height INT, weight INT, salary INT)")
+                            "teamID TEXT, Name TEXT, height INT, "+
+                            "weight INT, salary INT, season INT)")
     cur.execute(
-        "CREATE TABLE gameData(gameID INT, teamID TEXT, playerID INT, Name TEXT, minutes INT, fgm INT, " +
-        "fga INT, tpm INT, tpa INT, ftm INT, fta INT, oreb INT, dreb INT, " +
-        "reb INT, assist INT, steal INT, block INT, turnover INT, fouls INT, " +
-        "plus_minus INT, points INT, injury TEXT, pir FLOAT, npr FLOAT, UNIQUE(gameID, playerID))")
+        "CREATE TABLE gameData(gameID INT, teamID TEXT, playerID INT, "+
+                                "Name TEXT, minutes INT, fgm INT, " +
+                                "fga INT, tpm INT, tpa INT, ftm INT, "+
+                                "fta INT, oreb INT, dreb INT, " +
+                                "reb INT, assist INT, steal INT, "+
+                                "block INT, turnover INT, fouls INT,"+
+                                " plus_minus INT, points INT, "+
+                                "injury TEXT, pir FLOAT, npr FLOAT, "+
+                                "UNIQUE(gameID, playerID))")
+
 
 
 con.commit()
