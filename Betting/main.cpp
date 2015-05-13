2145e9dd8154e8074d66a01a39c479d076003794
jkholodnov <jkholodnov@gmail.com>
2015-05-13 19:20:01 -0400

    more_betting stuff

--- Betting/main.cpp
+++ Betting/main.cpp
@@ -10,8 +10,10 @@ int main(int argc, char** argv) {
 
     Database* the_db = new Database("../2015.db");
 
-    string team1elo_query = "SELECT currentELO FROM teams WHERE teamID = '" + string(argv[1]) + "';";
-    string team2elo_query = "SELECT currentELO FROM teams WHERE teamID = '" + string(argv[2]) + "';";
+    string team1elo_query =
+        "SELECT currentELO FROM teams WHERE teamID = '" + string(argv[1]) + "';";
+    string team2elo_query =
+        "SELECT currentELO FROM teams WHERE teamID = '" + string(argv[2]) + "';";
 
     auto team1result = the_db->query(team1elo_query);
     auto team2result = the_db->query(team2elo_query);
@@ -21,12 +23,31 @@ int main(int argc, char** argv) {
 
     double elo_difference = abs(team1elo - team2elo);
 
-    string all_games = "SELECT count(*) FROM games WHERE abs(team1elo - team2elo) > " + to_string(elo_difference - 10) + " AND abs(team1elo - team2elo) < " + to_string(elo_difference + 10) + ";";
-    string correctly_predicted_games = "SELECT count(*) FROM games WHERE abs(team1elo - team2elo) > " + to_string(elo_difference - 10) + 
-    									" AND abs(team1elo - team2elo) < " + to_string(elo_difference + 10) + 
-    									" AND ((team1elo > team2elo AND team1score > team2score) OR (team1elo < team2elo AND team1score < team2score);";
+    string all_games = "SELECT count(*) FROM games WHERE abs(team1elo - team2elo) > " +
+                       to_string(elo_difference - 10) +
+                       " AND abs(team1elo - team2elo) < " +
+                       to_string(elo_difference + 10) + ";";
+
+    string correctly_predicted_games =
+        "SELECT count(*) FROM games WHERE abs(team1elo - team2elo) > " +
+        to_string(elo_difference - 10) + " AND abs(team1elo - team2elo) < " +
+        to_string(elo_difference + 10) +
+        " AND ((team1elo > team2elo AND team1score > team2score) OR "
+        "(team1elo < team2elo AND team1score < team2score));";
+
+    auto all_games_within_elo_range = the_db->query(all_games);
+    auto correct_games_within_elo_range = the_db->query(correctly_predicted_games);
+
+    double percent_win =
+        stod(all_games_within_elo_range[0][0]) / stod(correctly_predicted_games[0][0]);
+
+    if (team1elo > team2elo) {
+        cout << argv[1] << " has a " << percent_win << " chance to win.";
+    } else {
+        cout << argv[2] << " has a " << percent_win << " chance to win.";
+    }
 
     cout << all_games << endl;
-  	cout << correctly_predicted_games << endl;
+    cout << correctly_predicted_games << endl;
     delete the_db;
 }
