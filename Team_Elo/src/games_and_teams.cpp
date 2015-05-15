308e9e4cd00bcb32550d4c3f28df7b495f90257e
jkholodnov <jkholodnov@gmail.com>
2015-05-14 20:38:55 -0400

    fixing teams scrape extra inserts

--- Team_Elo/src/games_and_teams.cpp
+++ Team_Elo/src/games_and_teams.cpp
@@ -1,10 +1,6 @@
 #include "../include/games_and_teams.h"
 
-games_and_teams::games_and_teams() {
-    cout << "doing stuff" << endl;
-    the_db = new Database("../2015.db");
-    cout << "doing stuff" << endl;
-}
+games_and_teams::games_and_teams() { the_db = new Database("../2015.db"); }
 
 games_and_teams::~games_and_teams() { delete the_db; }
 
